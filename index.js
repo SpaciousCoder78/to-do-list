a056521bf4f3e63e2a1df0d74be79180d1f23988
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2022-08-09 16:17:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function newtask(){
+    const text = document.getElementById("new")
+    const ask = prompt("Enter the description of task");
+    const para = document.createElement("label");
+    const broke =document.createElement("br")
+    var checkbox = document.createElement("INPUT");
+    checkbox.type="checkbox"
+    para.innerText = ask;
+    text.appendChild(para);
+    text.appendChild(checkbox)
+    text.appendChild(broke)
+}
+
