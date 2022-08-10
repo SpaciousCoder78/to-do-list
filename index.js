0d95a33299288550bacf244925e90af22a1fe0cf
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2022-08-10 11:21:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-
 function newtask(){
     const text = document.getElementById("new")
-    const ask = prompt("Enter the description of task");
+    const ask = document.getElementById("askingpart").value;
     const para = document.createElement("label");
     const broke =document.createElement("br")
     var checkbox = document.createElement("INPUT");
     checkbox.type="checkbox"
     para.innerText = ask;
     text.appendChild(para);
-    text.appendChild(checkbox)
-    text.appendChild(broke)
-    text.appendChild(broke)
+    text.appendChild(checkbox);
+    text.appendChild(broke);
+    text.appendChild(broke);
+
 }
 
